04bad61fd625380fbd59dbd7680797cd418e1325
Thomas B. Verderesi <93417818+tverderesi@users.noreply.github.com>
2024-12-16 22:42:34 -0300

    updated undefined check on fetchinput

--- util/fetchInput.js
+++ util/fetchInput.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const fetchInput = (input_path, split = undefined) => {
   const input = fs.readFileSync(input_path).toString();
-  if (split) {
+  if (split !== undefined) {
     return input.split(split);
   }
   return input;
